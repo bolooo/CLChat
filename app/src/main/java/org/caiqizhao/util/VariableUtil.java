d486b2d1d932e4f37b73179cedb0a48294110728
Heng Luo <bigboloo@163.com>
2018-12-29 21:07:34 +0800

    chats消息列表

--- app/src/main/java/org/caiqizhao/util/VariableUtil.java
+++ app/src/main/java/org/caiqizhao/util/VariableUtil.java
@@ -1,5 +1,5 @@
 package org.caiqizhao.util;
 
 public class VariableUtil {
-    public static String Service_IP = "http://172.22.155.4:8080/";
+    public static String Service_IP = "http://192.168.253.5:8080/";
 }
