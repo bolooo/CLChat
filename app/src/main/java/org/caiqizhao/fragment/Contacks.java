d1e3984a36fd5cb604bb493743c35775063c19b3
Cai Qizhao <1158537305@qq.com>
2018-12-29 14:32:11 +0800

    2018.12.28/////

--- app/src/main/java/org/caiqizhao/fragment/Contacks.java
+++ app/src/main/java/org/caiqizhao/fragment/Contacks.java
@@ -14,7 +14,7 @@ import com.example.bolo.chat.R;
 
 public class Contacks extends Fragment {
     private View view;
-    public static Context context;
+    public static Context context;/////
 
     @Override
     public void onAttach(Context context) {
