868c5411cac669cef640080a5b372e746463f46d
Cai Qizhao <1158537305@qq.com>
2019-01-01 20:10:58 +0800

    2019-1-1 消息状态

--- app/src/main/java/org/caiqizhao/activity/ChatView.java
+++ app/src/main/java/org/caiqizhao/activity/ChatView.java
@@ -1,7 +1,9 @@
 package org.caiqizhao.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +26,9 @@ import org.caiqizhao.service.addMessageService;
 import org.caiqizhao.util.VariableUtil;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -45,8 +50,11 @@ public class ChatView extends AppCompatActivity {
     private MsgAdapter adapter;
     public static UserFriend friend = null;
     private String friend_ip;
-    public static SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private   SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
     private Toolbar toolbar;
+    public static Handler handler;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,6 +64,16 @@ public class ChatView extends AppCompatActivity {
         msgList = Message.messageHasMap.get(friend.getFriend_id());
         getFriendIP();
         setChatView();
+        initMessageState();
+    }
+
+    //更新消息阅读状态
+    private void initMessageState() {
+        List<Message> messageList = Message.messageHasMap.get(friend.getFriend_id());
+        for(Message message:messageList){
+            message.setMessage_state(1);
+        }
+        System.out.println(Message.messageHasMap.get(friend.getFriend_id()));
     }
 
     private void getFriendIP() {
@@ -135,6 +153,7 @@ public class ChatView extends AppCompatActivity {
         toolbar.setTitle(friend.getFriend_name());
     }
 
+    //好友不在线，离线消息发送
     private class sendNotIPOnClick implements View.OnClickListener{
 
         @Override
@@ -156,6 +175,67 @@ public class ChatView extends AppCompatActivity {
                 intent.putExtra("json",new Gson().toJson(msg));
                 startService(intent);
             }
+        }
+    }
+
+
+    //好友在线，在线消息发送
+    private class sendIPOnClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            String content = inputText.getText().toString();
+            if (!"".equals(content)) {
+                Message msg = new Message();
+                msg.setMessage_state(1);
+                msg.setUser_id(User.user.getUser_id());
+                msg.setFriend_id(friend.getFriend_id());
+                msg.setTime(simpleDateFormat.format(new Date()));
+                msg.setMessage(content);
+                msg.setPut_id(1);
+                msgList.add(msg);
+                adapter.notifyItemInserted(msgList.size() - 1);
+                msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                inputText.setText("");
+                Intent intent = new Intent(ChatView.this, addMessageService.class);
+                final String json = new Gson().toJson(msg);
+                intent.putExtra("json", json);
+                startService(intent);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Socket socket = new Socket(friend_ip,9999);
+                            OutputStream out = socket.getOutputStream();
+                            out.write(json.getBytes());
+                            out.flush();
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        }
+    }
+
+
+
+    private class MessageUtil extends Handler{
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            super.handleMessage(msg);
+            Bundle data = msg.getData();
+            String str = data.getString("message");
+            Message masssage = new Gson().fromJson(str,Message.class);
+            masssage.setMessage_state(1);
+            msgList.add(masssage);
+            adapter.notifyItemInserted(msgList.size() - 1);
+            msgRecyclerView.scrollToPosition(msgList.size() - 1);
+            inputText.setText("");
+            Intent intent = new Intent(ChatView.this,addMessageService.class);
+            intent.putExtra("json",new Gson().toJson(msg));
+            startService(intent);
         }
     }
 }
--- app/src/main/java/org/caiqizhao/service/getFriendMessageService.java
+++ app/src/main/java/org/caiqizhao/service/getFriendMessageService.java
@@ -5,6 +5,18 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 
+import com.google.gson.Gson;
+
+import org.caiqizhao.activity.ChatView;
+import org.caiqizhao.entity.Message;
+
+import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class getFriendMessageService extends Service {
     private  LocalBinder mBinder;
 
@@ -23,5 +35,47 @@ public class getFriendMessageService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         return mBinder;
+    }
+
+    private void PortListener(){
+        try {
+            ServerSocket serverSocket = new ServerSocket(9999);
+            while (true){
+                serverSocket.accept();
+
+            }
+        }catch (Exception e){
+        }
+    }
+
+    private class getFriendMessageRun implements Runnable{
+        private Socket socket;
+
+        public getFriendMessageRun(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                int size;
+                byte[] bytes = new byte[1024*4];
+                InputStream inputStream = socket.getInputStream();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                while ((size = inputStream.read(bytes)) != -1) {
+                    baos.write(bytes, 0, size);
+                }
+                socket.close();
+                String str = baos.toByteArray().toString();
+                if(ChatView.friend == null){
+
+                }else {
+                    android.os.Message message = new android.os.Message();
+                    ChatView.handler.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
