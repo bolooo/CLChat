a3742971719a126361153bee3fe8b84cbac8254e
Cai Qizhao <1158537305@qq.com>
2019-01-02 23:41:04 +0800

    2019-1-2 最终版本测试

--- app/src/main/java/org/caiqizhao/activity/NewFriend.java
+++ app/src/main/java/org/caiqizhao/activity/NewFriend.java
@@ -6,9 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+
 
 import com.example.bolo.chat.R;
 
